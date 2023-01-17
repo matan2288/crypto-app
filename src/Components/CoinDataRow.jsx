d55e04089f939a6d1034a98abb7705a49957b217
matan2288 <matan2288@gmail.com>
2023-01-17 18:49:12 +0200

    added link to coin page

--- src/Components/CoinDataRow.jsx
+++ src/Components/CoinDataRow.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { URLS } from '../utils/consts'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import '../Style/Components/coin-data-row-style.scss'
 
 export const CoinDataRow = (props) => {
@@ -14,8 +15,10 @@ export const CoinDataRow = (props) => {
     <div className='coin-row-main-container'>
       <img src={URLS.cryptoCompareURL + ImageUrl} alt={Name} className='data-row-icon' />
       <div className='coin-names-container'>
-        <span className="coin-full-name">{FullName}</span>
-        <span className="coin-short-name">{Name}</span>
+        <Link to={`/coin/${ FullName.toLowerCase() }`}>
+          <span className="coin-full-name">{FullName}</span>
+          <span className="coin-short-name">{Name}</span>
+        </Link>
       </div>
       <span className='coin-price'>{PRICE}</span>
     </div>
